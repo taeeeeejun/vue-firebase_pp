2d2fd66242a22ea3a087427755d2ac84f7c152ff
taejun <pakrjun@gmail.com>
2020-08-09 20:45:12 +0900

    vuetify  적용

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
-  <div id="app">
-    타이틀 수정
-    <div id="nav">
-      <router-link to="/">Main</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon/>
+      <v-toolbar-title>Page Title</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
+  data: () => ({
+    //
+  })
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
