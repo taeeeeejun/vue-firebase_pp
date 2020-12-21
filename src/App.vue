0f51fe8dece92713d4cff31b5ba7311d4604f4ec
TaeJun Park <kr601435@amway.com>
2020-12-21 10:16:50 +0900

    fetching data for footer

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"/>
       <site-title :title="site.title"></site-title>
       <v-spacer/>
-      <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
-      <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
-      <v-btn icon @click="readOne"><v-icon>mdi-account-badge-alert</v-icon></v-btn>
       <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
@@ -41,7 +38,33 @@ export default {
     return {
       drawer: false,
       site: {
-        menu: [],
+        menu: [
+          {
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [
+              {
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [
+              {
+                title: 'xxx',
+                to: '/xxx'
+              }
+            ]
+          }
+        ],
         title: '나의 타이틀 입니다',
         footer: '푸터입니다.'
       }
@@ -56,27 +79,12 @@ export default {
         const v = sn.val()
         if (!v) {
           this.$firebase.database().ref().child('site').set(this.site)
+          return
         }
         this.site = v
       }, (error) => {
         console.log(error.message)
       })
-    },
-    save () {
-      console.log('save000')
-      this.$firebase.database().ref().child('abcd').set({
-        title: 'abcd', text: 'tttt'
-      })
-    },
-    read () {
-      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
-        console.log(sn)
-        console.log(sn.val())
-      })
-    },
-    async readOne () {
-      const sn = await this.$firebase.database().ref().child('abcd').once('value')
-      console.log(sn.val())
     }
   }
 }
--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <v-list-group
-        v-for="(item, index) in items"
-        :key="index"
-        v-model="item.active"
-        :prepend-icon="item.icon"
-        no-action
-      >
+    <v-list-item>
+      <v-list-content>
+        <v-list-item-title class="title">
+          Application
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          subtext
+        </v-list-item-subtitle>
+      </v-list-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-group
+          v-for="(item, index) in items"
+          :key="index"
+          v-model="item.active"
+          :prepend-icon="item.icon"
+          no-action
+        >
         <template v-slot:activator>
           <v-list-item-content>
             <v-list-item-title v-text="item.title"></v-list-item-title>
@@ -23,40 +35,15 @@
           </v-list-item-content>
         </v-list-item>
       </v-list-group>
+    </v-list>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['items'],
   data () {
     return {
-      items: [
-        {
-          title: 'home',
-          icon: 'mdi-home',
-          subItems: [
-            {
-              title: 'Dashboard',
-              to: '/'
-            },
-            {
-              title: 'About',
-              to: '/about'
-            }
-          ]
-        },
-        {
-          title: 'about',
-          active: true,
-          icon: 'mdi-account',
-          subItems: [
-            {
-              title: 'xxx',
-              to: '/xxx'
-            }
-          ]
-        }
-      ]
     }
   }
 }
