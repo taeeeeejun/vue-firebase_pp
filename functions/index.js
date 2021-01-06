46f3b8eb3ad2f72c96a915e1cec3639f02aa1cb8
TaeJun Park <kr601435@amway.com>
2021-01-06 16:51:51 +0900

    add paginate, createDate

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,7 @@ admin.initializeApp({
 });
 
 const db = admin.database()
+const fdb = admin.firestore()
 
 exports.createUser = functions.auth.user().onCreate(async (user) => {
   const { uid, email, displayName, photoURL } = user
@@ -27,3 +28,15 @@ exports.deleteUser = functions.auth.user().onDelete(async (user) => {
   const { uid } = user
   db.ref('users').child(uid).remove()
 });
+
+exports.incrementBoardCount = functions.firestore.document('boards/{bid}').onCreate(async (snap, context) => {
+  try {
+    await fdb.collection('meta').doc('boards').update('count', admin.firestore.FieldValue.increment(1))
+  } catch (error) {
+    await fdb.collection('meta').doc('boards').set({ count: 1 })
+  }
+})
+
+exports.decrementBoardCount = functions.firestore.document('boards/{bid}').onDelete(async (snap, context) => {
+  await fdb.collection('meta').doc('boards').update('count', admin.firestore.FieldValue.increment(-1))
+})
--- src/views/board/index.vue
+++ src/views/board/index.vue
@@ -1,61 +1,130 @@
 <template>
   <v-card>
     <v-card-title>board test</v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :items-per-page="5"
+      :options.sync="options"
+      :server-items-length="serverItemsLength"
+    >
+      <template v-slot:[`item.id`]="{ item }">
+        <v-btn icon @click="openDialog(item)"><v-icon>mdi-pencil</v-icon></v-btn>
+        <v-btn icon @click="remove(item)"><v-icon>mdi-delete</v-icon></v-btn>
+      </template>
+      <template v-slot:[`item.createdAt`]="{ item }">
+        {{ item.createdAt.toLocaleString() }}
+      </template>
+    </v-data-table>
     <v-card-actions>
-      <v-btn @click="read"><v-icon left>mdi-page-next</v-icon></v-btn>
-      <v-btn @click="openDialog"><v-icon left>mdi-pencil</v-icon></v-btn>
+      <v-btn @click="openDialog(null)"><v-icon left>mdi-pencil</v-icon></v-btn>
     </v-card-actions>
     <v-dialog max-width="500" v-model="dialog">
       <v-card>
-        <v-card-text>
-          <v-form>
-            <v-text-field v-model="form.title"></v-text-field>
-            <v-text-field v-model="form.content"></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn @click="save">save</v-btn>
-        </v-card-actions>
+        <v-form>
+          <v-card-text>
+              <v-text-field v-model="form.title"></v-text-field>
+              <v-text-field v-model="form.content"></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn @click="update" v-if="selectedItem">update</v-btn>
+            <v-btn @click="add" v-else>save</v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
     </v-dialog>
   </v-card>
 </template>
 <script>
+import { head, last } from 'lodash'
+
 export default {
   data () {
     return {
+      headers: [
+        { value: 'createdAt', text: '작성일' },
+        { value: 'title', text: '제목' },
+        { value: 'content', text: '내용' },
+        { value: 'id', text: 'id' }
+      ],
       items: [],
       form: {
         title: '',
         content: ''
       },
-      dialog: false
+      dialog: false,
+      selectedItem: null,
+      unsubscribe: null,
+      unsubscribeCount: null,
+      serverItemsLength: 0,
+      options: {},
+      docs: []
+    }
+  },
+  watch: {
+    options: {
+      handler (n, o) {
+        console.log(n)
+        console.log(o)
+        this.subscribe()
+      },
+      deep: true
+    }
+  },
+  destoryed () {
+    if (this.unsubscribe) {
+      this.unsubscribe()
     }
   },
   methods: {
-    openDialog () {
-      this.dialog = true
+    subscribe () {
+      this.unsubscribeCount = this.$firebase.firestore().collection('meta').doc('boards').onSnapshot((doc) => {
+        if (!doc.exists) {
+          return
+        }
+        this.serverItemsLength = doc.data().count
+      })
+      this.unsubscribe = this.$firebase.firestore().collection('boards').limit(this.options.itemsPerPage).onSnapshot((sn) => {
+        if (sn.empty) {
+          this.items = []
+          return
+        }
+        this.docs = sn.docs
+        console.log(head(sn.docs).data())
+        console.log(last(sn.docs).data())
+        this.items = sn.docs.map(v => {
+          const item = v.data()
+          return {
+            id: v.id, title: item.title, content: item.content, createdAt: item.createdAt.toDate()
+          }
+        })
+      })
     },
-    save () {
-      this.$firebase.firestore().collection('boards').add(this.form)
+    openDialog (item) {
+      this.selectedItem = item
+      this.dialog = true
+      if (!item) {
+        this.form.title = ''
+        this.form.content = ''
+      } else {
+        this.form.title = item.title
+        this.form.content = item.content
+      }
+    },
+    add () {
+      const item = {}
+      Object.assign(item, this.form)
+      item.createdAt = new Date()
+      this.$firebase.firestore().collection('boards').add(item)
       this.dialog = false
     },
-    async read () {
-      const sn = await this.$firebase.firestore().collection('boards').get()
-      sn.docs.forEach(v => {
-        console.log(v.id)
-        console.log(v.data())
-      })
-
-      this.items = sn.docs.map(v => {
-        const item = v.data()
-
-        return {
-          id: v.id, title: item.title, content: item.content
-        }
-      })
-      console.log(this.items)
+    update () {
+      this.$firebase.firestore().collection('boards').doc(this.selectedItem.id).update(this.form)
+      this.dialog = false
+    },
+    remove (item) {
+      this.$firebase.firestore().collection('boards').doc(item.id).delete()
     }
   }
 }
