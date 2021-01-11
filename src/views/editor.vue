2ceea9055f2f1d0802fa62fd2e8e48769f87890d
TaeJun Park <kr601435@amway.com>
2021-01-11 16:53:55 +0900

    create editor

---
+++ src/views/editor.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-sheet>
+        <v-subheader>
+            editor
+        </v-subheader>
+        <v-btn @click="toogle=!toogle">toogle</v-btn>
+        <v-btn @click="read">read</v-btn>
+        <editor :options="options" :initialValue="text" ref="editor"></editor>
+        <viewer v-if="toogle" :initialValue="text"></viewer>
+    </v-sheet>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      options: {
+        language: 'ko'
+      },
+      text: '# abcdefg',
+      toogle: false
+    }
+  },
+  methods: {
+    read () {
+      this.text = this.$refs.editor.invoke('getMarkdown')
+      alert(this.$refs.editor.invoke('getMarkdown'))
+    }
+  }
+}
+</script>
